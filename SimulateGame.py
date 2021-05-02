92be0213979958102fca3248e36873b467e6aaec
isamuelsson <46253408+isamuelsson@users.noreply.github.com>
2021-05-02 14:14:48 -0400

    Updated GA_Simulate for multiprocessing

--- SimulateGame.py
+++ SimulateGame.py
@@ -1,5 +1,9 @@
 from GameBroker import GameBroker
 import BoardPresets
+import multiprocessing as mp
+import random
+# from os import getpid
+import os
 
 def main(mutation_type, selection_type, reproduction_type, mutation_rate, is_testing):
 
@@ -26,8 +30,40 @@ def GA_simulate(matches):
     for match in enumerate(matches):
         game = GameBroker( match[0], match[1], max_search_time , initial_board = BoardPresets.silverman4x5())
         brokers.append(game)
-        
-    
 
+    initial_board = BoardPresets.silverman4x5()
+    max_search_time = 1
+
+    for broker in brokers:
+        print(broker.white_heuristic_coefficients,"vs",broker.black_heuristic_coefficients)
+
+    print("cpus available:",mp.cpu_count())
+
+    with mp.Pool(mp.cpu_count()-1) as pool:
+        
+        results = pool.map(sim_game, brokers) #pool.map ensures order is maintained
+
+        pool.close()
+        pool.join()
+
+        print("multiprocessing done")
+    
+    print(results)
+
+    for value in results:
+
+        if value == 1:
+            print("White Won!")
+        elif value == -1:
+            print("Black Won!")
+
+def sim_game(gameBroker):
+
+    print("starting process",os.getpid())
+
+    return gameBroker.simulate_game(verbose=False)
+
+    
+        
 if __name__ == "__main__":
     main()
