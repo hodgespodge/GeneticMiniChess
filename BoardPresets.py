ebda102ba1e8654b124bbd18ab06c64a0f0a5a36
root <root@DESKTOP-3U9D0GB.localdomain>
2021-05-05 20:09:36 -0400

    Fixed import error

--- BoardPresets.py
+++ BoardPresets.py
@@ -1,4 +1,5 @@
-from Board import Board, tuple_sub
+from Board import Board
+from MiscFunctions import tuple_sub
 
 # ["white_king","white_queen","white_rook","white_bishop","white_knight","white_pawn","black_king","black_queen","black_rook","black_bishop","black_knight","black_pawn"]
 
